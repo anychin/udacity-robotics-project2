16b76f46a9617d2d6ecbad32babe9e50cc8686a4
Anton Anuchin <anychin@gmail.com>
2019-02-28 02:55:13 +0300

    fixed a mistake

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -12,7 +12,7 @@ void drive_robot(float lin_x, float ang_z)
   drive.request.linear_x = lin_x;
   drive.request.angular_z = ang_z;
 
-  ROS_INFO("Robot are moving – linear_x:%1.2f, angular_z:%1.2f", lin_x, ang_z);
+  ROS_INFO("Robot are moving - linear_x:%1.2f, angular_z:%1.2f", lin_x, ang_z);
 
   if (!client.call(drive))
       ROS_ERROR("Failed to use drive_bot command");
@@ -23,7 +23,7 @@ void process_image_callback(const sensor_msgs::Image img)
 {
 
     int white_pixel = 255;
-    int pixel_position = 0;
+    int pixel_position = -1;
 
     for (int i = 0; i < img.height * img.step; i++) {
       if (img.data[i] == white_pixel) {
@@ -41,15 +41,15 @@ void process_image_callback(const sensor_msgs::Image img)
 
     // Go left
     if (pixel_position >= 0 && pixel_position < left_section)
-      drive_robot(0.1, 0.5);
+      drive_robot(0.1, 0.3);
 
     // Go Forward
     if (pixel_position > left_section && pixel_position <= right_section)
-      drive_robot(0.07, 0.0);
+      drive_robot(0.1, 0.0);
 
     // Go Right
     if (pixel_position > right_section)
-      drive_robot(0.1, -0.5);
+      drive_robot(0.1, -0.3);
 }
 
 int main(int argc, char** argv)
